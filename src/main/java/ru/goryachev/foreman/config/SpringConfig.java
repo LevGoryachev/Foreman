f9c0589c50405af5a0fbc2717203f5ccc3c3ddac
LevGoryachev <l.goryachev@inbox.ru>
2020-10-05 14:06:54 +0300

    Create class PartBean

--- src/main/java/ru/goryachev/foreman/config/SpringConfig.java
+++ src/main/java/ru/goryachev/foreman/config/SpringConfig.java
@@ -1,9 +1,16 @@
 package ru.goryachev.foreman.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.goryachev.foreman.service.PartBean;
 
 @Configuration
 public class SpringConfig {
+
+    @Bean
+    public PartBean getPartBean () {
+        return new PartBean("Foreman: test page");
+    }
 
 }
---
+++ src/main/java/ru/goryachev/foreman/service/PartBean.java
@@ -0,0 +1,21 @@
+package ru.goryachev.foreman.service;
+
+public class PartBean {
+
+    private String name;
+
+    public PartBean() {
+    }
+
+    public PartBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
