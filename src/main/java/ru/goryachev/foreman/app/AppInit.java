262028b2df8fde27ccb7860272fd1cdfcbae060a
Lev <l.goryachev@inbox.ru>
2020-10-21 14:44:08 +0300

    configuration: WebApp rename to AppInit, extends AbstractAnnotationServletInitializer

---
+++ src/main/java/ru/goryachev/foreman/app/AppInit.java
@@ -0,0 +1,24 @@
+package ru.goryachev.foreman.app;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import ru.goryachev.foreman.config.WebConfig;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{
+                WebConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
