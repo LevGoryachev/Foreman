5a31cbd43007a22cc1ff3f481686cb7a37fded47
Lev <l.goryachev@inbox.ru>
2020-12-23 20:38:41 +0300

    AppUserService: create getCurrentUser() (to get from user auth)

--- src/main/java/ru/goryachev/foreman/service/AppUserService.java
+++ src/main/java/ru/goryachev/foreman/service/AppUserService.java
@@ -1,6 +1,9 @@
 package ru.goryachev.foreman.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import ru.goryachev.foreman.dao.AppUsersDAO;
 import ru.goryachev.foreman.entities.AppUser;
@@ -37,4 +40,19 @@ public class AppUserService implements Applicable {
         return appUsersDAO.getById(id);
     }
 
+    public AppUser getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        Object obj = auth.getPrincipal();
+        String userLogin = "";
+
+        if (obj instanceof UserDetails) {
+            userLogin = ((UserDetails) obj).getUsername();
+        } else {
+            userLogin = obj.toString();
+        }
+
+        AppUser currentAppUser = appUsersDAO.getByLogin(userLogin);
+        return currentAppUser;
+    }
 }
